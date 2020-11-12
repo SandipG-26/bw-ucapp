b429fda057578c22e0b34454ba7cca4a9a014423
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-11-12 19:06:48 +0530

    all delete and add service button added

--- src/App.js
+++ src/App.js
@@ -3,46 +3,101 @@ import React from 'react';
 import './App.css';
 import Service from './Service/service'
 
-const App = props => {
- const [serviceState, setServiceState] = useState(
-   {
-      services :[
+const  App = props => {
+  const [serviceState, setServiceState] = useState(
+    {
+      services: [ 
         {service:"Door-Step" , Name:"Painting", Vendor:"Sandip", Area:"Pune"},
         {service:"Door-Step" , Name:"Repairing", Vendor:"Rohan", Area:"Mumbai"},
         {service:"Door-Step" , Name:"Insalling", Vendor:"Kiran", Area:"Delhi"},
-        {service:"Door-Step" , Name:"Cleaning", Vendor:"Akshay", Area:"Agra"}
-      ]
+        {service:"Door-Step" , Name:"Cleaning", Vendor:"Akshay", Area:"Agra"},
+               
+      ],
+      Count: 4,
+      showServices: true, 
+      showServicesLabel: "Hide Services"
     }
- );
+  );
+  
+  
 
-  const updateHandler = () => {
+  const addServiceHandler = () => {
+    const serviceLists = [ ...serviceState.services];
+    const newService = {service: "Door-Step", Name:"Degree", Vendor:"dinesh", Area:"BAngalore"}
+    serviceLists.push(newService);
+
     setServiceState({
-      services :[
-            {service:"Door-Step" , Name:"Painting", Vendor:"a", Area:"Pune"},
-            {service:"Door-Step" , Name:"Repairing", Vendor:"b", Area:"Mumbai"},
-            {service:"Door-Step" , Name:"Insalling", Vendor:"c", Area:"Delhi"},
-            {service:"Door-Step" , Name:"Cleaning", Vendor:"d", Area:"Agra"}
-          ]
+            services: serviceLists, 
+            Count : serviceState.Count +1,
+            showServices: serviceState.showServices,
+            showServicesLabel: serviceState.showServicesLabel     
+      });
+  }
+
+  const deleteServiceHandler = (serviceIndex) => {
+    const serviceLists = [ ...serviceState.services];
+    serviceLists.splice(serviceIndex, 1); 
+    setServiceState({
+      services: serviceLists,   
+      Count : serviceState.Count -1,
+      showServices: serviceState.showServices,
+      showServicesLabel: serviceState.showServicesLabel     
     });
   }
 
-  
-  return (
-      <div className="App container-fluid">
-        <div className="row">
-            <h1>Lists Of Vehicles</h1>
-            <div className="buttonDiv">
-              <button className="btn btn-primary btn-md buttonUpdate" onClick={updateHandler}>Update List</button>
-            </div>
-        </div>
-        <div className="row">
-          <Service service={serviceState.services[0].service} Name={serviceState.services[0].Name} Vendor={serviceState.services[0].Vendor} Area={serviceState.services[0].Area} /> 
-          <Service service={serviceState.services[1].service} Name={serviceState.services[1].Name} Vendor={serviceState.services[1].Vendor} Area={serviceState.services[1].Area} />
-          <Service service={serviceState.services[2].service} Name={serviceState.services[2].Name} Vendor={serviceState.services[2].Vendor} Area={serviceState.services[2].Area} />
-          <Service service={serviceState.services[3].service} Name={serviceState.services[3].Name} Vendor={serviceState.services[3].Vendor} Area={serviceState.services[3].Area} />
-        </div>
-      </div>
-    )  
-};
+  const toggleServicesHandler= () => {
+    let toggleService = !serviceState.showServices;
+    let label = "";
+    
+    if(toggleService){ 
+      label= "Hide Services";
+    }
+    else{
+      label="Show Services";
+    }
+    setServiceState({
+      services: serviceState.services,
+      Count: serviceState.Count,
+      showServices: toggleService,
+      showServicesLabel: label
 
+    })
+  }
+  let serviceLists= null;
+  if(serviceState.showServices){
+    serviceLists=(<div className="row">
+    {
+      serviceState.services.map((serviceItem, index) => 
+      {
+      return <Service 
+      service={serviceItem.service}
+      Name={serviceItem.Name} 
+      Vendor={serviceItem.Vendor}  
+      Area={serviceItem.Area}
+      onDelete={deleteServiceHandler.bind(this, index)} 
+      /> 
+      })
+    }
+    </div>
+    );
+  } 
+
+  return (
+    
+      <div className="App container-fluid">
+        
+        <div className="row">
+            <div className = "col-lg-6">
+              <h1>Lists Of Vehicles l Total Vehicles:{serviceState.Count}</h1>
+            </div>
+            <div className="buttonDiv col-lg-6">
+              <button className="btn btn-primary btn-md buttonUpdate" onClick={addServiceHandler}>Add Service</button>
+              <button className="btn btn-secondary btn-md" onClick={toggleServicesHandler}>{serviceState.showServicesLabel}</button>
+            </div>
+        </div> 
+       {serviceLists}
+      </div>
+   
+ )  
+}
 export default App;
--- src/Service/service.js
+++ src/Service/service.js
@@ -3,10 +3,11 @@ import './service.css'
 const service = props => (
     <div class="type">
       <h1>Service: {props.service}</h1>
-      <p>Name: {props.Name} </p>
+      <p onClick="  constructor(props.updateClick">Name: {props.Name} </p>
       <p>Vendor: {props.Vendor} </p>
       <p>Area: {props.Area}</p>
-  </div>
+      <button className="btn btn-danger btn-sm" onClick={props.onDelete}> Delete</button>
+    </div>
 );
 
 export default service;

0df0c478a573b5df6ae5fc5b59a24e21292c2cb3
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-11-12 09:53:17 +0530

    update using the function is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import {useState} from 'react';
 import React from 'react';
 import './App.css';
 import Service from './Service/service'
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
+const App = props => {
+ const [serviceState, setServiceState] = useState(
+   {
       services :[
         {service:"Door-Step" , Name:"Painting", Vendor:"Sandip", Area:"Pune"},
         {service:"Door-Step" , Name:"Repairing", Vendor:"Rohan", Area:"Mumbai"},
@@ -14,39 +13,36 @@ class App extends React.Component {
         {service:"Door-Step" , Name:"Cleaning", Vendor:"Akshay", Area:"Agra"}
       ]
     }
-  }
+ );
 
-  updateHandler = () => {
-    this.state = {
+  const updateHandler = () => {
+    setServiceState({
       services :[
-        {service:"Door-Step" , Name:"Painting", Vendor:"Sandip", Area:"Pune"},
-        {service:"Door-Step" , Name:"Repairing", Vendor:"Rohan", Area:"Mumbai"},
-        {service:"Door-Step" , Name:"Insalling", Vendor:"Kiran", Area:"Delhi"},
-        {service:"Door-Step" , Name:"Cleaning", Vendor:"Akshay", Area:"Agra"}
-      ]
-    }
+            {service:"Door-Step" , Name:"Painting", Vendor:"a", Area:"Pune"},
+            {service:"Door-Step" , Name:"Repairing", Vendor:"b", Area:"Mumbai"},
+            {service:"Door-Step" , Name:"Insalling", Vendor:"c", Area:"Delhi"},
+            {service:"Door-Step" , Name:"Cleaning", Vendor:"d", Area:"Agra"}
+          ]
+    });
   }
 
-  render(){
-    
-    return (
+  
+  return (
       <div className="App container-fluid">
         <div className="row">
             <h1>Lists Of Vehicles</h1>
             <div className="buttonDiv">
-              <button className="btn btn-primary btn-md buttonUpdate" onClick="">Update List</button>
+              <button className="btn btn-primary btn-md buttonUpdate" onClick={updateHandler}>Update List</button>
             </div>
         </div>
         <div className="row">
-          <Service service={this.state.services[0].service} Name={this.state.services[0].Name} Vendor={this.state.services[0].Vendor} Area={this.state.services[0].Area} /> 
-          <Service service={this.state.services[1].service} Name={this.state.services[1].Name} Vendor={this.state.services[1].Vendor} Area={this.state.services[1].Area} />
-          <Service service={this.state.services[2].service} Name={this.state.services[2].Name} Vendor={this.state.services[2].Vendor} Area={this.state.services[2].Area} />
-          <Service service={this.state.services[3].service} Name={this.state.services[3].Name} Vendor={this.state.services[3].Vendor} Area={this.state.services[3].Area} />
+          <Service service={serviceState.services[0].service} Name={serviceState.services[0].Name} Vendor={serviceState.services[0].Vendor} Area={serviceState.services[0].Area} /> 
+          <Service service={serviceState.services[1].service} Name={serviceState.services[1].Name} Vendor={serviceState.services[1].Vendor} Area={serviceState.services[1].Area} />
+          <Service service={serviceState.services[2].service} Name={serviceState.services[2].Name} Vendor={serviceState.services[2].Vendor} Area={serviceState.services[2].Area} />
+          <Service service={serviceState.services[3].service} Name={serviceState.services[3].Name} Vendor={serviceState.services[3].Vendor} Area={serviceState.services[3].Area} />
         </div>
       </div>
-    )
-  }
-  
-}
+    )  
+};
 
 export default App;
